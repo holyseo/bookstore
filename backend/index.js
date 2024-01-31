aab16308acdcf197b6ae844770fc068232cb8068
holyseo <128103221+holyseo@users.noreply.github.com>
2024-01-31 11:31:05 +0000

    add UPDATE a book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -44,6 +44,42 @@ app.get("/books", async (req, res) => {
   }
 });
 
+app.get("/book/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const book = await Book.findById(id);
+    return res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+app.put("/book/:id", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Please include all fileds: title, author, publishYear",
+      });
+    }
+    const { id } = req.params;
+
+    const result = await Book.findByIdAndUpdate(id, req.body);
+    console.log(result);
+
+    if (!result) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    return res.status(200).json({ message: "Book updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    if (error instanceof mongoose.CastError) {
+      return res.status(404).json({ message: "invalid id" });
+    }
+    res.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(MongoDBURL)
   .then(() => {
