ee4fac600abd987cac83b57e5bfe3fbaf46fc10b
holyseo <128103221+holyseo@users.noreply.github.com>
2024-01-31 11:43:32 +0000

    add DELETE book with id

--- backend/index.js
+++ backend/index.js
@@ -80,6 +80,20 @@ app.put("/book/:id", async (req, res) => {
   }
 });
 
+app.delete("/book/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Book.findByIdAndDelete(id);
+    if (!result) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    return res.status(200).send({ message: "Book deleted successfully" });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
   .connect(MongoDBURL)
   .then(() => {
