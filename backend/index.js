860d820a9a93be8aecbd91b64f47fae405ffb429
holyseo <128103221+holyseo@users.noreply.github.com>
2024-01-31 10:36:17 +0000

    set up express, mongodb and test the first dataset

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import { PORT, MongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Welcome to MERN Stack Bookstore");
+});
+
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Please include all fileds: title, author, publishYear",
+      });
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+    return res.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(MongoDBURL)
+  .then(() => {
+    console.log(`App connected to MongoDB`);
+    app.listen(PORT, () => {
+      console.log(`App is listening to  port: ${PORT} the server 🚀`);
+    });
+  })
+  .catch((error) => console.log(error));
