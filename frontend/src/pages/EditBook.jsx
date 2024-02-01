5062482b54da13fbcb21c1731dec50b253d28293
holyseo <128103221+holyseo@users.noreply.github.com>
2024-02-01 14:46:54 +0000

    add Edit Book

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,5 +1,92 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import BackButton from "../components/BackButton";
+
 const EditBook = () => {
-  return <div>EditBook</div>;
+  const { id } = useParams();
+  const [book, setBook] = useState({ title: "", author: "", publishYear: "" });
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get(`http://localhost:5555/books/${id}`).then(({ data }) => {
+      setBook(data);
+    });
+  }, [id]);
+
+  const updateBook = (e) => {
+    const { name, value } = e.target;
+    setBook((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+
+    const newBook = {
+      title: book.title,
+      author: book.author,
+      publishYear: book.publishYear,
+    };
+
+    console.log(newBook);
+
+    axios
+      .put(`http://localhost:5555/books/${id}`, newBook)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="p-4 bg-gray-400 bg-opacity-50 w-fit mx-auto my-10 ">
+      <div className="text-lg font-bold m-5">Edit book details</div>
+      <form onSubmit={handleEdit} className="flex flex-col gap-5 ">
+        <div className="flex flex-row justify-between items-center gap-5">
+          <label>Title:</label>
+          <input
+            type="text"
+            className="p-1 my-1 rounded-md"
+            name="title"
+            value={book.title}
+            onChange={updateBook}
+          />
+        </div>
+        <div className="flex flex-row justify-between items-center gap-5">
+          <label>Author:</label>
+          <input
+            type="text"
+            className="p-1 my-1 rounded-md"
+            name="author"
+            value={book.author}
+            onChange={updateBook}
+          />
+        </div>
+        <div className="flex flex-row justify-between items-center gap-5">
+          <label>Publish Year:</label>
+          <input
+            type="text"
+            className="p-1 my-1 rounded-md"
+            name="publishYear"
+            value={book.publishYear}
+            onChange={updateBook}
+          />
+        </div>
+        <div className="flex flex-row justify-between items-center">
+          <BackButton />
+          <button
+            type="submit"
+            className="text-lg font-bold bg-blue-800 h-12 text-white w-1/3 rounded-md hover:bg-amber-600"
+          >
+            Edit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default EditBook;
